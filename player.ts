93b07789e07cde8888e57d62ed91551e82ff5e57
George <george.ot.dev@gmail.com>
2019-12-04 14:23:20 +0200

    recovered comments

--- player.ts
+++ player.ts
@@ -23,5 +23,15 @@ export class Player
     Defend(incomingThrow:number, opponent:Player):boolean
     {
         return incomingThrow < this.skill;
+        // if(incomingThrow > this.skill) {
+        //     console.log(this.name + " Didn't Manage To Defend Succesfully, " + opponent.name + "Gets The Point");
+        //     opponent.score++;
+
+        //     return false;
+        // } 
+
+        // console.log(this.name + " Managed To Defend Succesfully");
+        // this.score++;
+        // return true;
     }
 }
